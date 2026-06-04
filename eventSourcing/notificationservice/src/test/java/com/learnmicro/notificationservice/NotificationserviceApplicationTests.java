8bdd6a01919435fbc3a3ad21f5a276d312d6fbee
PC <dangtrungdoee2bk@gmail.com>
2026-06-04 21:57:45 +0700

    #22. Create notification service

---
+++ eventSourcing/notificationservice/src/test/java/com/learnmicro/notificationservice/NotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnmicro.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
